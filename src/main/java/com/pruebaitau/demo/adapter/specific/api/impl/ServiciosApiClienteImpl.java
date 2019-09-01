9a5fdf3b22252f68ac92b6cf8cecde744e219f16
jdrodriguez <jdrodriguez>
2019-09-01 13:54:02 -0500

    Se agrega validacion

--- src/main/java/com/pruebaitau/demo/adapter/specific/api/impl/ServiciosApiClienteImpl.java
+++ src/main/java/com/pruebaitau/demo/adapter/specific/api/impl/ServiciosApiClienteImpl.java
@@ -64,7 +64,9 @@ public class ServiciosApiClienteImpl extends EndpointManagerAbstract implements 
     public ResponseEntity viewListItemsConsume(String id, String title) {
 
             String url;
-            if (!ObjectUtils.isEmpty(id) && StringUtils.isEmpty(title)){
+            if (!ObjectUtils.isEmpty(id) && !StringUtils.isEmpty(title)) {
+                url = "https://my-project-itau.firebaseio.com/content.json?orderBy=\"id\"&equalTo=\""+id+"\"&print=pretty";
+            }else if (!ObjectUtils.isEmpty(id) && StringUtils.isEmpty(title)){
                 url = "https://my-project-itau.firebaseio.com/content.json?orderBy=\"id\"&equalTo=\""+id+"\"&print=pretty";
             }else if (!StringUtils.isEmpty(title) && ObjectUtils.isEmpty(id)){
                 url = "https://my-project-itau.firebaseio.com/content.json?orderBy=\"title\"&equalTo=\""+title+"\"&print=pretty";
